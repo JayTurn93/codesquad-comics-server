c6e4cd73c3a0d02b120aadeaff1b595d41d529d5
JayTurn93 <jyturner93@gmail.com>
2025-05-13 22:11:43 -0400

    cp-server-1 complete

---
+++ app.js
@@ -0,0 +1,88 @@
+const express = require("express");
+const morgan = require("morgan");
+const path = require("node:path");
+const helmet = require("helmet");
+const cors = require("cors");
+const app = express();
+const PORT = 8080;
+//-------------MIDDLEWARE---------------
+app.use(helmet());
+app.use(morgan("dev"));
+app.use(cors());
+app.use(express.urlencoded());
+app.use(express.json());
+app.use(express.static(path.join(__dirname + "/public")));
+
+//------------SEND ROUTES----------------
+// app.get("/", (request, response, next) => {
+//     response.send("This route points to the Home page")
+// });
+
+// app.get("/api/books", (request, response, next) => {
+//     response.send("This will send all of the book data")
+// });
+
+// app.get("/api/books/:id", (request, response, next) => {
+//     response.send("This will send a single book by its id")
+// });
+
+// app.get("/api/books/create/new", (request, response, next) => {
+//     response.send("This will create a new book")
+// });
+
+// app.get("/api/books/update/:id", (request, response, next) => {
+//     response.send("This will update a book by its id")
+// });
+
+// app.get("/api/books/delete/:id", (request, response, next) => {
+//     response.send("This will delete a book by its id")
+// });
+
+
+//-------------REFACORED-----------------
+app.get("/", (request, response, next) => {
+    response.status(200).json({
+        success: { message: "This route points to the Home page" },
+        statusCode: 200,
+      });
+});
+
+app.get("/api/books", (request, response, next) => {
+    response.status(200).json({
+        success: { message: "This will send all of the book data" },
+        statusCode: 200,
+      });
+});
+
+app.get("/api/books/:id", (request, response, next) => {
+    response.status(200).json({
+        success: { message: "This will send a single book by its id" },
+        statusCode: 200,
+      });
+});
+
+app.get("/api/books/create/new", (request, response, next) => {
+    response.status(200).json({
+        success: { message: "This will create a new book" },
+        statusCode: 200,
+      });
+});
+
+app.get("/api/books/update/:id", (request, response, next) => {
+    response.status(200).json({
+        success: { message: "This will update a book by its id" },
+        statusCode: 200,
+      });
+});
+
+app.get("/api/books/delete/:id", (request, response, next) => {
+    response.status(200).json({
+        success: { message: "This will delete a book by its id" },
+        statusCode: 200,
+      });
+});
+
+app.listen(PORT, () => {
+    console.log(`The server is listening on port ${PORT}`)
+    console.log(`(http://localhost:${PORT}/`)
+});
