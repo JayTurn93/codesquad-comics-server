509784c328f9be29918ddc305df6d6d265a67236
JayTurn93 <jyturner93@gmail.com>
2025-05-24 23:50:31 -0400

    completed step 1 of the server 3 part 1 assignment. contiuning to step 2.

---
+++ models/userModel.js
@@ -0,0 +1,34 @@
+const mongoose = require("mongoose");
+const { Schema } = mongoose;
+
+const userSchema = new Schema({
+    firstName: {
+        type: String,
+    },
+    lastName: {
+        type: String,
+    },
+    username: {
+        type: String,
+        required: true,
+        unique: true,
+        trim: true,
+        lowercase: true,
+    },
+    password: {
+        type: String,
+        required: true,
+        minLength: 8,
+
+    },
+    googleId: {
+        type: String,
+    },
+    githubId: {
+        type: String,
+    },
+});
+
+const User = mongoose.model("User", userSchema);
+
+module.exports = User;
