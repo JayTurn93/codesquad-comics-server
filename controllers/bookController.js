9a74e364743f21cb516622af82fcbc81fea3d4d0
JayTurn93 <jyturner93@gmail.com>
2025-05-28 00:18:20 -0400

    part 2 started. defintiely show have pushed before now :/

--- controllers/bookController.js
+++ controllers/bookController.js
@@ -1,77 +1,53 @@
-const booksData = require("../data/books");
-
+// const booksData = require("../data/books");
+const Book = require("../models/bookModel");
 
 const getAllBooks = async (request, response, next) => {
-    
+
     try {
-        const books = booksData;
+        // const books = booksData;
+        const books = await Book.find({});
         return response.status(200).json({
             success: {message: "We got all the books."},
-            data: {books: books},
+            data: {books},
             statusCode: 200,
         });
     } catch (error) {
-        return response.status(400).json({
-            error: {message: "There was a problem retrieving the books"},
-            statusCode: 400,
-        });
-    }
-}
+        return next(error)
+    };
+};
 
 const getBook = async (request, response, next) => {
     const {_id} = request.params;
 
     try {
-        const foundBook = booksData.find((book) => book._id === _id);
+        // const foundBook = booksData.find((book) => book._id === _id);
+
+        if (!_id) {
+            throw new Error("ID required.")
+        }
+        const book = Book.findById(_id);
+        if (!book) {
+            throw new Error("Book not found.")
+        }
         return response.status(200).json({
             success: {message: "We have located the book by ID"},
             data:{data: foundBook},
             statusCode: 200,
         });
     } catch (error) {
-        return response.status(400).json({
-            error: {message: "Error trying to get the book by ID"},
-            statusCode: 400,
-        });
+        return next(error)
     };
 };
 
 const createBook = async (request, response, next) => {
     const {title, author, publisher, genre, pages, rating, synopsis, imageUrl} = request.body;
-    const newBook = {
-        title, 
-        author,
-        publisher,
-        genre,
-        pages,
-        rating,
-        synopsis,
-        imageUrl,
-    };
-
-    try {
-    
-    return response.status(201).json({
-        success: {message: "We added a new comic!"},
-        data: {newBook},
-        statusCode: 201,
-    });
-    } catch (error) {
-        return response.status(400).json({
-            error: {message: "There was a problem creating the comic"},
-            statusCode: 400,
-        });
-    };
-};
-
-const updateBook = async (request, response, next) => {
-    const {_id} = request.params;
-    const { title, author, publisher, genre, pages, rating, synopsis, imageUrl } = request.body;
-    
     
     try {
-        const updatedBook = {
-            title,
+        if (!title || !author || !pages) {
+            throw new Error("Enter missing fields.")
+        }
+        const newBook = new Book({
+            title, 
             author,
             publisher,
             genre,
@@ -79,37 +55,68 @@ const updateBook = async (request, response, next) => {
             rating,
             synopsis,
             imageUrl,
-        };
-        
+        });
+        await newBook.save();
+        return response.status(201).json({
+            success: {message: "We added a new comic!"},
+            data: {newBook},
+            statusCode: 201,
+        });
+    } catch (error) {
+        return next(error)
+    };
+};
+
+const updateBook = async (request, response, next) => {
+    const {_id} = request.params;
+    const { title, author, publisher, genre, pages, rating, synopsis, imageUrl } = request.body;
+    try {
+        if (!title || !author || !pages) {
+          throw new Error("Enter missing fields.")
+        }
+        const updatedBook = await Book.findByIdAndUpdate(_id,
+            {
+                $set: {
+                  title,
+                  author,
+                  publisher,
+                  genre,
+                  pages,
+                  rating,
+                  synopsis,
+                  imageUrl,
+                },
+            },
+            {new: true}
+        );
+        if (!updatedBook) {
+            throw new Error("Book not updated.")
+        }
         return response.status(201).json({
             success: {message: "We updated the comic!"},
             data: {updatedBook},
             statusCode: 201,
         });
     } catch (error) {
-        return response.status(400).json({
-            error: {message: "Theres a problem updating the comic"},
-            statusCode: 400,
-        });
+        return next(error)
     };
 };
 
 const deleteBook = async (request, response, next) => {
     const {_id} = request.params;
-
     try {
-        const books = booksData.filter((book) => book._id !== _id);
+        if (!_id) {
+            throw new Error("ID required.")
+        }
+        // const books = booksData.filter((book) => book._id !== _id);
+        await Book.findByIdAndDelete(_id);
         
         return response.status(200).json({
             success:{message: "We have deleted that comic."},
-            data: {books},
             statusCode: 200,
         });
     } catch (error) {
-        return response.status(400).json({
-            error: {message: "There was a problem deleting the comci."},
-            statusCode: 400,
-        });
+        return next(error)
     };
 };
 
