d3687a51a6f047b50317bf632a0faf1c0c94c801
JayTurn93 <jyturner93@gmail.com>
2025-06-04 22:15:29 -0400

    entering testing for authtication and books

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,70 +1,119 @@
 const { request, response } = require("express");
+const passport = require("passport");
+const bcrypt = require("../models/userModel");
+const User = require("../models/userModel");
+
+
+const register = async (request, response, next) => {
+    const {firstName, lastName, usenName, password, googleId, githubId} = request.body;
+    console.log(request.body);
+    if (error) {
+        return next(error);
+    } else if (!firstName || !username || !password) {
+        return response.status(400).json({
+            error: {message: "Missing required fields."},
+            statusCode: 400,
+        })
+    }
+        try {
+            const hashedPassword = await bcrypt.hash(password, 10);
+            const newUser = new User ({
+                firstName: firstName,
+                lastName: lastName,
+                username: username,
+                password: hashedPassword,
+                googleId: "",
+                githubId: githubId,
+            });
+        
+            await newUser.save();
+
+            request.login(newUser, (error) => {
+                if (error) {
+                    return next(error);
+                }
+
+                newUser.password = undefined;
+
+                return response.status(201).json({
+                    success: {message: "New user created."},
+                    data: {newUser},
+                    statusCode: 201
+                });
+            })
+            
+        } catch (error) {
+            if (error.code === 11000 && error.keyPattern.username) {
+                response.status(400).json({
+                    error: {message: "Username already exist."},
+                    statusCode: 400,
+                });
+            } else {
+              response.status(500).json({
+              error: {message: "Internal server error"},
+              statusCode: 500
+             });
+            }
+        };
+    };
+
+
 const login = async (request, response, next) => {
-    response.status(200).json({
-        success: {message: "User logged in successfully"},
+    try {
+        return response.status(200).json({
+        success: {message: "Login Successfil"},
         statusCode: 200,
     });
+    } catch (error) {
+        return response.status(400).json({
+            error: {message: "There was a problem logging in."},
+            statusCode: 400
+        });
+    };
 };
 
 const localLogin = async (request, response, next) => {
-    const result = true;
+    const userCopy = { ...request.user._doc };
+    userCopy.password = undefined;
 
-    function mockPassport (error, user) {
+    passport.authenticate("local", (error, user, info) => {
         if (error) {
-            return next(error);
+            return next (error)
+        };
+        if (!user) {
+            return response.status(401).json({
+                error: {message: "There is no user detected. Try again"},
+            });
         }
-    }
-    mockPassport();
-    return response.json({
-        success: {message: "Login Successful"},
-        result: {result},
+        const userCopy = {...request.user._doc};
+        userCopy.password = undefined;
+        console.log(userCopy);
+        response.status(200).json({
+            success: {message: "Local Login Complete"},
+            data: {user: userCopy},
+            statusCode: 200,
+        });
     })
 };
 
 const logout = async (request, response, next) => {
-    console.log("Initializing logout controller logic...");
-    console.log("session destroyed");
-    response.clearCookie("connect.sid");
-    
-
-    return response.status(200).json({
-        success: {message:"User logged out"},
-        statusCode: 200,
-        });
-    function sessionDestruction (error) {
-    //error handling as a final check and a failsafe
-    if (error) {
-      return next(error);
-    }
-  sessionDestruction();
-  console.log("Logout function activated");
-  }
-};
-
-const register = async (request, response, next) => {
-    const { firstName, lastName, userName, password } = request.body;
-    console.log(firstName, lastName, userName, password);
-
-    try {
-        const newUser = {
-            firstName,
-            lastName,
-            userName, 
-            password,
+    request.logout((error) => {
+        if (error) {
+            return next(error);
         };
-        console.log("In motion");
-        response.status(201).json({
-            success: {message: "New user created."},
-            data: {newUser},
-            statusCode: 201
+        request.session.destroy((error) => {
+            if (error) {
+                return next(error);
+            }
         })
-    } catch (error) {
-        response.status(500).json({
-            error: { message: "Internal Server error!"},
-            statusCode: 500
+        response.clearCookie("connect.sid");
+        return response.status(200).json({
+            success: {message: "User logged out!"},
+            statusCode: 200,
         })
-    }
+    })
 };
 
 
-module.exports = { register, login, logout, localLogin };
+
+module.exports = { register, logout, localLogin, login };
--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,20 +1,29 @@
 const express = require("express");
 const router = express.Router();
+const passport = require("passport");
 
 const { register, login, logout, localLogin} = require("../controllers/authController");
 
 router.post("/register", register);
-router.get("/login", login);
+router.post("/login", login,
+    passport.authenticate("local", {
+        failureRedirect: "/login/error",
+        failureMessage
+    })
+);
 router.get("/login/error", (request, response, next) => {
     return response.status(400).json({
         message: "Login error"
     })
 });
-router.get("/login/local", localLogin);
-router.get("/logout", logout);
+router.post("/login/local", localLogin);
+router.post("/logout", logout);
 router.get("/unauthenticated", (request, response, next) => {
     console.log("Returning to homepage...");
     response.redirect("/");
 });
-
+router.get("/login/google", passport.authenticate("google", {scope: ["profile", "email"] })
+);
+router.get("/login/callback", passport.authenticate("google", {failureRedirect: "/login", successRedirect: "/dashboard"})
+);
 module.exports = router;
